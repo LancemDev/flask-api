854dbd46be3647d1fd068933cc525539ce140811
LancemDev <munyaolance1@gmail.com>
2024-01-18 19:37:43 +0300

    :)

--- api/index.py
+++ api/index.py
@@ -2,6 +2,11 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello, World!'
+
+
 @app.route('/api', methods=['GET'])
 def api():
     return jsonify({'message': 'Hello, World!'})
