e20e1ce59f27fbf334ec1c6d934cfbe4cadb8f80
LancemDev <munyaolance1@gmail.com>
2024-01-18 19:31:19 +0300

    initial commmit

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api():
+    return jsonify({'message': 'Hello, World!'})
+
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    return jsonify(request.json)
+
+if __name__ == '__main__':
+    app.run(debug=True)
